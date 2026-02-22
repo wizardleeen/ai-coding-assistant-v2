13fc93332a010e861f402955caf8038da4205e08
wizardleeen <wizardleen@gmail.com>
2026-02-22 16:14:40 +0800

    修复静态文件部署问题 - 将 JS 和 CSS 内联到 HTML 中

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: {
-        main: 'index.html'
-      },
-      output: {
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name].[ext]'
-      }
-    },
-    copyPublicDir: false,
-    assetsDir: ''
-  },
-  publicDir: false
+      input: 'index.html'
+    }
+  }
 })
